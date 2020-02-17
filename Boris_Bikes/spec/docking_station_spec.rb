99267eaefbcf81624ec111752ff9d8b1f16ab27e
Victoria Tranfield <victoria.tranfield@gmail.com>
2020-02-17 16:42:03 +0000

    added bikes and docking station

---
+++ Boris_Bikes/spec/docking_station_spec.rb
@@ -0,0 +1,11 @@
+# In spec/docking_station_spec.rb
+require "docking_station"
+
+describe DockingStation do
+  it { is_expected.to respond_to :release_bike }
+
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+end
